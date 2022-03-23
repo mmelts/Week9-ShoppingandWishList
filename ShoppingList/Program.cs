9743716e7399124f85e60059f4f44f2b290782ce
Kristo Toomsoo <kristo@lteed.ee>
2022-03-23 22:48:03 +0200

    add new projects

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\KristoToomsoo\samples\wishList";
+            Console.WriteLine("Enter directory name: 'shopping list'");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name: 'myshoppinglist.txt'");
+            string fileName = "myshoppinglist.txt";
+
+            
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+               Console.WriteLine("Directory and File exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
